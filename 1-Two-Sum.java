ea4d7c23bdb0f1db5404f5ca360a71144cfa3db2
Pratik Shende <pratikshende99@gmail.com>
2022-07-08 15:45:50 +0530

    leetcode #1

---
+++ 1-Two-Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] res = new int[2];
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        for(int i = 0; i<nums.length; i++){
+            indexMap.put(nums[i],i);
+        }
+
+        for(int j = 0; j< nums.length; j++){
+            int newTarget = target-nums[j];
+            if(indexMap.containsKey(newTarget) && indexMap.get(newTarget)!=j){
+                res[0]= j;
+                res[1]= indexMap.get(newTarget);
+                return res;
+            }
+        }
+	    return res;
+    }
+}
