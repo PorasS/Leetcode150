5410945056b2ba632b4d84325aaab4d504d3ce19
Pratik Shende <pratikshende99@gmail.com>
2022-12-19 09:46:50 +0530

    Leet Code 150

---
+++ 150-Evalulate-Reverse-Polish-Notation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String token : tokens){
+            if(token.equals("+") || token.equals("-") || 
+            token.equals("*") || token.equals("/")){
+                int op2 = stack.pop();
+                int op1 = stack.pop();
+                int res = 0;
+                if(token.equals("+")){
+                    res = op1 + op2;
+                }else if(token.equals("-")){
+                    res = op1 - op2;
+                }else if(token.equals("/")){
+                    res = op1 / op2;
+                }else if(token.equals("*")){
+                    res = op1 * op2;
+                }
+                stack.push(res);
+            }else{
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return !stack.isEmpty() ? stack.pop() : 0;
+        
+    }
+}
