f2c6bb8dd3d44b802d9f9ab0f288ba47de8616bf
Pratik Shende <pratikshende99@gmail.com>
2023-05-29 09:04:02 +0530

    0104-maximum-depth-binary-tree.java

---
+++ 0104-maximum-depth-binary-tree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public int maxDepth(TreeNode root) {
+    return computeMaxDepth(root);
+  }
+
+  public int computeMaxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    return Math.max(computeMaxDepth(root.left), computeMaxDepth(root.right)) + 1;
+  }
+}
