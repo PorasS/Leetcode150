c7543fc736c835d12e2acc6f596bfdb9afc84098
Pratik Shende <pratikshende99@gmail.com>
2024-07-07 12:39:39 +0530

    0684-redundant-connection.java

--- 0684-redundant-connection.java
+++ 0684-redundant-connection.java
@@ -1,62 +1,46 @@
 class Solution {
-
-  List < List < Integer >> resList = null;
-  int[] nodes = null;
-  int[] sz = null;
+  int[] rank = null;
+  int[] vertices = null;
   public int[] findRedundantConnection(int[][] edges) {
-    //Weighted union find
-    nodes = new int[edges.length + 1];
-    sz = new int[edges.length + 1];
 
-    for (int i = 0; i < nodes.length; i++) {
-      nodes[i] = i;
-      sz[i] = 1;
-    }
+    rank = new int[edges.length + 1];
 
-    //nodes[1,2,3,4,5]
-    resList = new ArrayList < > ();
+    Arrays.fill(rank, 1);
 
-    for (int j = 0; j < edges.length; j++) {
-      union(edges[j][0], edges[j][1]);
+    vertices = new int[edges.length + 1];
+
+    for (int i = 0; i < vertices.length; i++) {
+      vertices[i] = i;
     }
 
     int[] res = new int[2];
+    for (int j = 0; j < edges.length; j++) {
+      int[] edge = edges[j];
+      int par1 = parent(edge[0]);
+      int par2 = parent(edge[1]);
 
-    if (!resList.isEmpty()) {
-      List < Integer > redudantEdge = resList.get(resList.size() - 1);
-      res[0] = redudantEdge.get(0);
-      res[1] = redudantEdge.get(1);
+      if (par1 == par2) {
+        res[0] = edge[0];
+        res[1] = edge[1];
+        return res;
+      }
+
+      if (rank[par1] >= rank[par2]) {
+        vertices[par2] = par1;
+        rank[par1] += 1;
+      } else {
+        vertices[par1] = parent(par2);
+        rank[par2] += 1;
+      }
     }
 
     return res;
   }
 
-  public void union(int p, int q) {
-    int pRoot = getRoot(p);
-    int qRoot = getRoot(q);
-
-    //p and q are connected, if we add an edge between p and q , will create a cycle
-    if (pRoot == qRoot) {
-      List < Integer > list = new ArrayList < > ();
-      list.add(p);
-      list.add(q);
-      resList.add(list);
+  public int parent(int ch) {
+    while (ch != vertices[ch]) {
+      ch = vertices[ch];
     }
-
-    if (sz[pRoot] >= sz[qRoot]) {
-      nodes[qRoot] = pRoot;
-      sz[pRoot] += sz[qRoot];
-    } else {
-      nodes[pRoot] = qRoot;
-      sz[qRoot] += sz[pRoot];
-    }
+    return ch;
   }
-
-  public int getRoot(int node) {
-    while (nodes[node] != node) {
-      node = nodes[node];
-    }
-    return node;
-  }
-
 }
