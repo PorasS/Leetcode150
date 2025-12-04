6082a321d9fbff7e362b226f93d4a8561f5bf735
Pratik Shende <pratikshende99@gmail.com>
2025-12-04 11:52:17 +0530

    0322-Coin-Change.java

--- 0322-Coin-Change.java
+++ 0322-Coin-Change.java
@@ -1,40 +1,34 @@
 class Solution {
-    
+
     int[][] dp;
+
     public int coinChange(int[] coins, int amount) {
         dp = new int[coins.length][amount + 1];
-        int minCoins = dfs(coins.length - 1, amount, coins);
-        return  minCoins == (int) 1e9 ? -1 : minCoins;
+        int minCoins = minCoins(coins, coins.length - 1, amount);
+        return minCoins == (int) 1e9 ? -1 : minCoins;
     }
 
+    public int minCoins(int[] coins, int idx, int target) {
 
-    public int dfs(int idx, int target, int[] coins){
-
-        if(idx >= 0 && target >= 0 && dp[idx][target] != 0){
-            return dp[idx][target];
+        if (idx < 0 || target < 0) {
+            return (int) 1e9;
         }
 
-        if(idx == 0){
-            if(target % coins[idx] == 0){
-                return target / coins[idx];
-            }else{
-                return (int)1e9;
-            }
-        }
-
-        if(target < 0){
-            return (int)1e9;
-        }
-
-        if(target == 0){
+        if (target == 0) {
             return 0;
         }
 
-        int noTake = 0 + dfs(idx - 1, target, coins);
-        int take = 1 + dfs(idx, target - coins[idx], coins);
+        if (dp[idx][target] > 0) {
+            return dp[idx][target];
+        }
 
-        int minCoins = Math.min(take, noTake);
-        dp[idx][target] = minCoins;
+        int take = 1 + minCoins(coins, idx, target - coins[idx]);
+        int noTake = 0 + minCoins(coins, idx - 1, target);
+
+        dp[idx][target] = Math.min(take, noTake);
+
         return dp[idx][target];
+
     }
+
 }
